5c36bee745dfa167e02de7b31b96f485394a2fe5
Binjie Zhuang <binjie.zhuang@dena.com>
2015-02-27 18:42:12 +0800

    further design some unit detail

---
+++ Project/Assets/Scripts/GameDatas/UnitData.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Base attack type
+public enum UnitWeaponType{
+	Sharp,
+	Blunt,
+	ArrowSingle,
+	ArrowMuilt,
+	MagicSingle,
+	MagicMuilt
+}
+
+// Base enchant type
+public enum UnitAttackEnchant{
+	NoEnchant,
+	Fire,
+	Ice,
+	Thunder,
+	Natural,
+	Holy,
+	Curse,
+	Poison
+}
+
+// Base defence type
+public enum UnitArmorType{
+	NoArmor,
+	LightArmor,
+	HeavyArmor,
+	EnhancedArmor
+}
+
+public class UnitData
+{
+	public int unit_id{ get; set;}
+	public List<int> talents{ get; set;}
+	public List<int> skills{ get; set;}
+	public int exp{ get; set;}
+}
+
