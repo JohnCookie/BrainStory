8b55cec53dea3960a00bbeec60c09ea5799776a6
JohnCookie <cookiejj2010@gmail.com>
2015-03-25 02:10:03 +0800

    something for monster list

---
+++ Project/Assets/Scripts/Items/UserMonsterBoxItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserMonsterBoxItem : MonoBehaviour {
+	UISprite m_spriteMonster;
+	UISprite m_spriteFrame;
+	UILabel m_labelLv;
+
+	void Awake(){
+		m_spriteMonster = transform.FindChild("monster").GetComponent<UISprite>();
+		m_spriteFrame = transform.FindChild("frame").GetComponent<UISprite>();
+		m_labelLv = transform.FindChild("lv").GetComponent<UILabel>();
+	}
+
+	public void OnMonsterItemClick(){
+
+	}
+}
---
+++ Project/Assets/Scripts/Pages/UserMonsterBoxPage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserMonsterBoxPage : MonoBehaviour {
+	UIButton m_btnBack;
+	UIScrollView m_scrollMonsters;
+	UIGrid m_gridMonsters;
+	GameObject m_monsterItemObj;
+
+	void Awake(){
+		m_btnBack = transform.FindChild("BackBtn").GetComponent<UIButton>();
+		m_scrollMonsters = transform.FindChild("MosnterScrollView").GetComponent<UIScrollView>();
+		m_gridMonsters = m_scrollMonsters.transform.FindChild("MonsterGrid").GetComponent<UIGrid>();
+		m_monsterItemObj = m_gridMonsters.transform.FindChild("MiniMonsterShower").gameObject;
+	}
+}
